5a2d892ccc40c558101009a037227d17e69d6568
danilo@danilo-PC <danilo@danilo-PC>
2018-04-05 18:00:03 -0500

    Se avanza hasta video 11

---
+++ client/reduxstagram.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { render } from "react-dom";
+
+// Import css
+import css from "./styles/style.styl";
+
+// import Components
+import App from "./components/App";
+import Single from "./components/Single";
+import PhotoGrid from "./components/PhotoGrid";
+
+// import react router deps
+import { Router, Route, IndexRoute } from "react-router";
+import { Provider } from "react-redux";
+import store, { history } from './Store';
+
+const router = (
+    <Provider store={store}>
+    <Router history={history}>
+        <Route path="/" component={App}>
+            <IndexRoute component={PhotoGrid}></IndexRoute>
+            <Route path="/view/:postId" component={Single}></Route>
+        </Route>
+    </Router>
+    </Provider>
+)
+
+render(router, document.querySelector("#root"));
